d9e9e8874b7ceb8ad4dba61acdc1976b969c5102
Srinivasan Kanniwadi <srinivasankh@gmail.com>
2025-03-10 21:41:19 +0000

    initial01

---
+++ script.js
@@ -0,0 +1,12 @@
+// You can add JavaScript for interactive elements here.
+// For example, smooth scrolling to sections:
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
